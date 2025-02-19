c03e92bca71ab8b9740d4e3db173ad161443717a
Pavan Gowda T S <pavangowdats01@gmail.com>
2025-02-19 12:23:49 +0530

    update server.js file

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-const express = require('express');
-const connectDB = require('./config/dp.connection');
-const authRoutes = require('./routes/auth');
+const app = require('./appRoutes')
 require('dotenv').config();
-
-const app = express();
-connectDB();
-app.use(express.json());
-app.use('/api',authRoutes);
 const PORT = process.env.PORT || 3000
-
-
-app.get("/", (req, res) => {
-    res.json({ message: "API is running..." });
-});
 
 
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
 })
 
-module.exports = app;
